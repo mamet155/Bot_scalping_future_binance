80af19659180207da04413a7ed897be492812d03
mamet155 <rahamadash83@gmail.com>
2026-01-07 07:42:24 +0700

    Create mode.py

---
+++ mode.py
@@ -0,0 +1,8 @@
+MODE = "DEMO"
+
+if MODE == "DEMO":
+    BASE_URL = "https://testnet.binancefuture.com"
+    MODE_NAME = "DEMO (TESTNET)"
+else:
+    BASE_URL = "https://fapi.binance.com"
+    MODE_NAME = "REAL (LIVE)"
